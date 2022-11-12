1daa5215f5f856225999d790265d14ef099f5abb
Filip Vrba <filipvrba@pm.me>
2022-11-12 22:29:11 +0100

    implement vweb

--- lib/mrbweb.v
+++ lib/mrbweb.v
@@ -1,5 +1,25 @@
 module main
 
+import vweb
+import os
+
+const (
+    public_dir = 'public'
+)
+
+struct App {
+    vweb.Context
+}
+
 fn main() {
-	println('Hello World!')
+	vweb.run_at(new_app(), vweb.RunParams{
+        port: 8081
+    }) or { panic(err) }
+}
+
+fn new_app() &App {
+    mut app := &App{}
+    dir_path := os.resource_abs_path('./$public_dir')
+    app.mount_static_folder_at(dir_path, public_dir)
+    return app
 }
