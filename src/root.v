03bb8fb0c87eac2b813b7a274dee4b898aa914e6
Filip Vrba <filipvrba@pm.me>
2022-11-14 17:07:55 +0100

    add function args for puts

--- src/root.v
+++ src/root.v
@@ -2,9 +2,11 @@ module main
 
 import vweb
 
-// arg &C.mrb_kwargs this is crucial for given method arguments!
 fn (mut a App) func_p_result (mrb &C.mrb_state, self C.mrb_value) C.mrb_value {
-	a.code_result += "[C] fn p_result\n"
+	
+	result := get_args(mrb, self)
+	a.code_result += "$result\n"
+	
 	return self
 }
 
